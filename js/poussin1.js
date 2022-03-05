29c06c1957b8425e8645627bcda143a0fa64e43d
Lea <lea.delaporte96@hotmail.fr>
2022-03-05 15:46:26 +0100

    meteo

--- js/poussin1.js
+++ js/poussin1.js
@@ -12,12 +12,5 @@ displayDate.innerHTML = currentTime.getDate() + "/";
 displayMonth.innerHTML = currentTime.getMonth() + "/";
 displayYear.innerHTML = currentTime.getFullYear();
 
-const APIKEY = 'd13cf80cbcf4869d611bb2f2c45896d1';
 
-let url = `https://api.openweathermap.org/data/2.5/weather?q=Amiens&appid=${APIKEY}&units=metric&lang=fr`
-
-fetch(url).then((response) =>
-    response.json().then((data) => 
-        console.log(data))
-);
     
